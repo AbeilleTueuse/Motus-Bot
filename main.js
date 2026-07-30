04e3fce021cb21a677280431028ee0bae19dc498
AbeilleTueuse <baptiste.ferrato1@orange.fr>
2026-07-30 23:07:55 +0200

    anti nathalie

--- main.js
+++ main.js
@@ -361,9 +361,9 @@ async function startGame() {
   const gameState = initializeGameStateFromGrid();
   const score = getTotalScore();
 
-  // if (score >= getPlayerScore("marius") + 5_000) {
-  //   return;
-  // }
+  if (score >= getPlayerScore("nathalie") + 5_000) {
+    return;
+  }
 
   // if (score >= 450_000) {
   //   return;
